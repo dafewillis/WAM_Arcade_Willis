26944d4ba1dfa409570fcaf539ffd972d7d97c46
dafewillis <94135833+dafewillis@users.noreply.github.com>
2022-01-13 16:44:48 -0700

    changes thursday 1/13 late afternoon

--- mole.js
+++ mole.js
@@ -3,18 +3,47 @@ const scoring = document.querySelectorAll('.score') //creating variable for my s
 const montgomeries = document.querySelectorAll('.montgomery') //creating variable that's a bucket for all my montgomeries
 
 let lastReactor;
+let gameOver = false;
+let score = 0;
 
-function popupTime (min, max) { //function to get random times for my montgomeries to pop up from the reactors
-    return Math.random() * (max - min) + min;
+function popupTime(min, max) { //function to get random times for my montgomeries to pop up from the reactors
+    return Math.round(Math.random() * (max - min) + min);
 }
 
-function randReactor (reactors) { //function to choose random reactors from which montgomeries will pop up
+function randReactor(reactors) { //function to choose random reactors from which montgomeries will pop up
     const idx = Math.floor(Math.random() * reactors.length);
     const reactor = reactors[idx];
-
-    console.log(reactor);
+    if (reactor === lastReactor) {
+        return randReactor(reactors);
+    }
 
     lastReactor = reactor;
+    return reactor;
 }
 
-//adding some changes to test commit and push -D.Willis
+function surprise() {
+    const time = popupTime (500, 1000);
+    const reactor = randReactor(reactors);
+    reactor.classList.add('up');  
+    setTimeout(() => {
+        reactor.classList.remove('up');
+        if (!gameOver) surprise();
+    }, time);  
+}
+
+function gameOn() {
+    scoring.textContent = 0;
+    gameOver = false;
+    score = 0;
+    surprise();
+    setTimeout(() => gameOver = true, 15000);
+}
+
+function thwack(e) {
+    if(!e.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoring.textContent = score;
+}
+
+montgomeries.forEach(montgomery => montgomery.addEventListener('click', thwack));
